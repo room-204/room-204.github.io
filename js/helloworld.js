270f8a728b27568a413de3c04d9b609ea0f50c3a
sssszzww <jbmok66@naver.com>
2022-07-01 18:45:15 +0900

    feat: Use vanila javascript instead

--- js/helloworld.js
+++ js/helloworld.js
@@ -1,5 +1,9 @@
-$(document).ready(function(){
-  $('.your-class').slick({
-    setting-name: setting-value
-  });
-}); 
+function damaY(){
+  var dama = document.getElementById('bul');
+  if(dama.scroll.match("dama_y")){
+    dama.src = "./img/jun_1.png";
+  }
+  else{
+    dama.src = "./img/2.png";
+  }
+}
